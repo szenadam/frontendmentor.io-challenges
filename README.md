127d998e929bfed20912e0d4211e53fcebce7aac
szenadam <szenadam1@gmail.com>
2022-04-13 16:17:32 +0200

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Frontendmentors.io Challenges
---
+++ interactive-rating-component-main/main.js
@@ -0,0 +1,6 @@
+const submit = document.getElementById('submit');
+const thankyouSection = document.getElementById('thankyou');
+
+submit.addEventListener('click', function(e) {
+  thankyouSection.style.display = 'block';
+})
