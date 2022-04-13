2ebdfe34ed7f90fe842e8970d68ec1edbceaf74f
szenadam <szenadam1@gmail.com>
2022-04-13 20:15:30 +0200

    Finish styling thank you state

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -1,12 +1,12 @@
-const submit = document.getElementById("submit");
+let selected = '';
 
-// 
 const scores = document.querySelectorAll(".score");
 scores.forEach((scoreElement) => {
   scoreElement.addEventListener("click", function () {
     scores.forEach((s) => {
       s.classList.remove("selected");
     });
+    selected = scoreElement.textContent;
     scoreElement.classList.add("selected");
   });
 });
@@ -15,5 +15,8 @@ function onSubmit() {
   const startState = document.getElementById("start-state");
   const thankyouSection = document.getElementById("thankyou");
   startState.style.display = 'none';
-  thankyouSection.style.display = 'block';
+  thankyouSection.style.display = 'flex';
+  const selectedText = `You selected ${selected} out of 5`;
+  const selectedElement = document.getElementById('selected-element');
+  selectedElement.innerText = selectedText;
 }
