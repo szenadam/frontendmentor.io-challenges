eb3aa915087cfff843c1a1af9f798a09123c9c88
szenadam <szenadam1@gmail.com>
2023-02-07 16:32:28 +0100

    01 Create desktop version

--- 01-product-preview-card-component/src/App.jsx
+++ 01-product-preview-card-component/src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import desktopImage from './assets/image-product-desktop.jpg'
 
 function App() {
   return (
     <>
-      <h1>Works</h1>
+      <div className="card">
+        <div className="card-image">
+          <img src={desktopImage} alt="Image of a cologne"/>
+        </div>
+        <div className="card-text">
+          <p className="top-title">PERFUME</p>
+          <h1 className="title">Gabrielle Essence Eau De Parfum</h1>
+          <p className="description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <div className="prices">
+            <span className="discount-price">$149.99</span>
+            <span className="original-price">$169.99</span>
+          </div>
+          <button className="add-to-cart-btn">Add to cart</button>
+        </div>
+      </div>
     </>
   )
 }
