2012cb44417c481691e5b76a16874aa407748ff1
szenadam <szenadam1@gmail.com>
2023-03-23 21:55:41 +0100

    03 Init project, finish half of the card component

---
+++ 03-results-summary-component/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="card">
+      <div className="left">
+        <h1>Your result</h1>
+        <div className="score">
+          <span>76 </span>
+          <span>of 100</span>
+        </div>
+        <div>Great</div>
+        <p>Your performance exceed 65% of the people conducting the test here!</p>
+      </div>
+      <div className="right">
+        <h1>Summary</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
