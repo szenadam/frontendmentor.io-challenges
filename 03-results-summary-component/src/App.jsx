d3fb31be81b075c70a2a7bd42042a2253ffee7c6
szenadam <szenadam1@gmail.com>
2023-03-26 11:12:23 +0200

    03 Finish desktop design

--- 03-results-summary-component/src/App.jsx
+++ 03-results-summary-component/src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css'
+import {ReactComponent as MemorySvg} from './assets/images/icon-memory.svg';
+import {ReactComponent as ReactionSvg} from './assets/images/icon-reaction.svg';
+import {ReactComponent as VerbalSvg} from './assets/images/icon-verbal.svg';
+import {ReactComponent as VisualSvg} from './assets/images/icon-visual.svg';
 
 function App() {
+
+  const SummaryRow = (props) => {
+    return (
+      <div className="summary-row" style={{backgroundColor: `var(${props.color})`}}>
+        <span className="summary-name" style={{color: `var(${props.color.replace("-pale", "")})`}}>{props.icon} {props.text}</span>
+        <span className="summary-score"><strong>{props.score}</strong> / 100</span>
+      </div>
+    );
+  }
 
   return (
     <div className="card">
@@ -15,7 +28,15 @@ function App() {
       </div>
       <div className="right">
         <h1>Summary</h1>
+        <SummaryRow icon={<ReactionSvg/>} text="Reaction" score="80" color="--light-red-pale"/>
+        <SummaryRow icon={<MemorySvg/>} text="Memory" score="92" color="--orangey-yellow-pale"/>
+        <SummaryRow icon={<VerbalSvg/>} text="Verbal" score="61" color="--green-teal-pale"/>
+        <SummaryRow icon={<VisualSvg/>} text="Visual" score="73" color="--cobalt-blue-pale"/>
+        <div className="continue-btn">
+          Continue
+        </div>
       </div>
+
     </div>
   )
 }
