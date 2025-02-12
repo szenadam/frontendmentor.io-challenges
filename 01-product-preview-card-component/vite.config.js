029ff5c2079a4899cfd108162c233510e1c2e4c5
szenadam <szenadam1@gmail.com>
2025-02-12 11:31:56 +0100

    01 Add base url in vite build fo CI

--- 01-product-preview-card-component/vite.config.js
+++ 01-product-preview-card-component/vite.config.js
@@ -5,4 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '01-product-preview-card-component/dist',
 })
