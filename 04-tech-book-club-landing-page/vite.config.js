2ba8468055cb4c6b1db0924e9add7494ed7dbf1c
szenadam <szenadam1@gmail.com>
2025-02-13 12:17:23 +0100

    04 Fix fonts and images vite resolutions

--- 04-tech-book-club-landing-page/vite.config.js
+++ 04-tech-book-club-landing-page/vite.config.js
@@ -1,6 +1,14 @@
+import { resolve } from "path";
+
 export default {
-    css: {
-        devSourcemap: true,
-    },
-    base: 'frontendmentor.io-challenges/04-tech-book-club-landing-page/dist',
+  css: {
+    devSourcemap: true,
+  },
+  base: 'frontendmentor.io-challenges/04-tech-book-club-landing-page/dist',
+  resolve: {
+    alias: {
+      $fonts: resolve("./public/assets/fonts/"),
+      $images: resolve("./public/assets/images/"),
+    }
+  }
 }
